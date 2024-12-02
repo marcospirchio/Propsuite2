e3d406c0997371b8437f3b544bf81cbeaba82e4b
Marcos Pirchio <Pirchiomarcos@gmail.com>
2024-12-02 03:01:42 -0300

    Delete src directory

--- src/main/java/com/example/AdministracionEdificiosTpApis/data/TipoReclamoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AdministracionEdificiosTpApis.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.AdministracionEdificiosTpApis.model.TipoReclamo;
-
-public interface TipoReclamoRepository extends JpaRepository<TipoReclamo, Integer> {
-
-}
